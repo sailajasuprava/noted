6b8ef640639222a30ca73b52b2e16dd3d5be70b2
sailajasuprava <sailaja.suprava@gmail.com>
2025-02-19 08:25:44 +0530

    authentication backend

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+// const { protect } = require("../controllers/authController");
+
+const router = express.Router();
+
+// router.use(protect);
+
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:userID").get(getUser).patch(updateUser).delete(deleteUser);
+
+module.exports = router;
