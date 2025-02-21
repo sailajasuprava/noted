6613832fac58ae89509367acf56ab2578ba8a47f
sailajasuprava <sailaja.suprava@gmail.com>
2025-02-21 09:28:08 +0530

    navbar dropdown menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import useLogout from "../hooks/useLogout";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { RiArrowDropDownLine } from "react-icons/ri";
 import { useState } from "react";
 import { FaPenAlt } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
@@ -10,12 +11,13 @@ function Navbar() {
   const { auth } = useAuth();
   const { isLoading, handleLogout } = useLogout();
   const [showMenu, setShowMenu] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <header className="flex justify-between w-screen bg-white px-8 py-2 fixed z-[10]">
+    <header className="flex justify-between w-screen px-8 py-2 fixed z-[10]">
       <Link to="/">
-        <div className="font-serif font-bold text-3xl flex gap-2">
-          Noted
+        <div className="font-serif font-bold text-3xl flex gap-1">
+          Noted.
           <span>
             <FaPenAlt />
           </span>
@@ -33,13 +35,35 @@ function Navbar() {
 
       <ul className={`${showMenu ? "nav-mobile" : "nav-desk"}`}>
         {auth ? (
-          <button
-            className="btn-black"
-            onClick={handleLogout}
-            disabled={isLoading}
-          >
-            Log out
-          </button>
+          <div className="relative">
+            <button
+              className="flex items-center bg-gray-200 p-2 pr-4 rounded-md"
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              <span>
+                <RiArrowDropDownLine size={30} />
+              </span>
+              <span>Hi, {auth.fullname}</span>
+            </button>
+
+            {dropdownOpen && (
+              <div className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-lg">
+                <Link to="/write">
+                  <li className="block px-4 py-2 hover:bg-gray-100">Write</li>
+                </Link>
+                <Link to="/profile">
+                  <li className="block px-4 py-2 hover:bg-gray-100">Profile</li>
+                </Link>
+                <button
+                  className="btn-black w-full rounded-none"
+                  onClick={handleLogout}
+                  disabled={isLoading}
+                >
+                  Log out
+                </button>
+              </div>
+            )}
+          </div>
         ) : (
           <>
             <NavLink to="/signup">
