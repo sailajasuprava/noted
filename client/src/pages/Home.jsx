4389acbf97ad0235b3ff220a31ecf460f3cbc812
sailajasuprava <sailaja.suprava@gmail.com>
2025-02-26 10:05:45 +0530

    fetch by category

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,29 @@
 import BlogList from "../components/BlogList";
 import FilterChipBar from "../components/FilterChipBar";
+import useHome from "../hooks/useHome";
 
 function Home() {
+  const {
+    active,
+    blogs,
+    hasMore,
+    isLoading,
+    fetchAllBlogs,
+    handleCategoryClick,
+  } = useHome();
+
   return (
     <div className="px-12 pt-8">
-      <FilterChipBar />
-      <BlogList />
+      <FilterChipBar
+        active={active}
+        handleCategoryClick={handleCategoryClick}
+      />
+      <BlogList
+        blogs={blogs}
+        hasMore={hasMore}
+        isLoading={isLoading}
+        fetchAllBlogs={fetchAllBlogs}
+      />
     </div>
   );
 }
